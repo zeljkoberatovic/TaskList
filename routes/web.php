aab9e859e7ba7c9167bf8ca6b759751a4b23beef
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-06 12:20:16 +0200

    testiranje git-a i projekta

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return 'Main Page';
+});
+
+Route::get('/zeka', function () {
+    return 'Hello Zeka';
+});
+
+//Dodavanje dinamickih djelova na samom url-u
+Route::get('/greet/{name}', function($name) {
+    return 'Helo' . $name . '!';
+});
