762d9ee94525649242eb1cc11a1fc1722ca0039c
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-12 14:06:14 +0200

    Blade directiva

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,17 @@
 <h2>Heloo Blade templete</h2>
 
-<!-- Direktiv isset nam vrsi provjeru odredjene varijable,da li je definisana -->
+<!-- Direktiv isset nam vrsi provjeru odredjene varijable,da li je definisana 
 @isset($name)
-    
-
-<h1>This is name: {{ $name }}</h1>
+    <h1>This is name: {{ $name }}</h1>
 @endisset
+-->
+
+<div>
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are not task!</div>
+    @endforelse
+</div>
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,72 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//inprovizovana baza podataka i njeni podaci za test koriscenje
+class Task
+{
+  public function __construct(
+    public int $id,
+    public string $title,
+    public string $description,
+    public ?string $long_description,
+    public bool $completed,
+    public string $created_at,
+    public string $updated_at
+  ) {
+  }
+}
+
+$tasks = [
+  new Task(
+    1,
+    'Buy groceries',
+    'Task 1 description',
+    'Task 1 long description',
+    false,
+    '2023-03-01 12:00:00',
+    '2023-03-01 12:00:00'
+  ),
+  new Task(
+    2,
+    'Sell old stuff',
+    'Task 2 description',
+    null,
+    false,
+    '2023-03-02 12:00:00',
+    '2023-03-02 12:00:00'
+  ),
+  new Task(
+    3,
+    'Learn programming',
+    'Task 3 description',
+    'Task 3 long description',
+    true,
+    '2023-03-03 12:00:00',
+    '2023-03-03 12:00:00'
+  ),
+  new Task(
+    4,
+    'Take dogs for a walk',
+    'Task 4 description',
+    null,
+    false,
+    '2023-03-04 12:00:00',
+    '2023-03-04 12:00:00'
+  ),
+];
+//sad smo anonimnu funkciju postavili da je dostupna
+Route::get('/', function () use ($tasks) {
     return view('index',[
         //'name' => 'Zeljko'
+        'tasks' => $tasks
     ]);
-});
+})->name('tasks.index');
 
-Route::get('/zeka', function () {
-    return 'Hello Zeka';
-});
+Route::get('/{id}', function ($id) {
+    return 'One single task!';
+})->name('tasks.show');
+
+
 
 //Dodavanje dinamickih djelova na samom url-u
 Route::get('/greet/{name}', function($name) {
