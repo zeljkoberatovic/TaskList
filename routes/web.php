b6bf9d271f4371ffc82611d9d6233e3cb8cf7008
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-14 01:19:45 +0200

    Forms and csfr protection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 Use Illuminate\Http\Response;
+Use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,86 +15,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//inprovizovana baza podataka i njeni podaci za test koriscenje
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
-
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-//sad smo anonimnu funkciju postavili da je dostupna
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index',[
-        //'name' => 'Zeljko'
-        'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')
+     ->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-        if(!$task){
-            abort(Response::HTTP_NOT_FOUND);
-        }
-    return view('show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+      'task' => \App\Models\Task::findOrFail($id)
+    ]);
 })->name('tasks.show');
 
+Route::post('/tasks', function (Request $request) {
+  dd($request->all());
+})->name('tasks.store');
 
 
-//Dodavanje dinamickih djelova na samom url-u
-Route::get('/greet/{name}', function($name) {
-    return 'Helo' . $name . '!';
-});
+
 
 
