3a36e228a3f86c9ea7883f59086db9cd71d834a9
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-19 10:17:49 +0200

    Validating and Storing Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+Use App\Models\Task;
 Use Illuminate\Http\Response;
 Use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,23 +20,38 @@ Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
+
 Route::get('/tasks', function () {
     return view('index',[
-        'tasks' => \App\Models\Task::latest()->where('completed', true)->get()
+        'tasks' => Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
+
 
 Route::view('/tasks/create', 'create')
      ->name('tasks.create');
 
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
-      'task' => \App\Models\Task::findOrFail($id)
+      'task' => Task::findOrFail($id)
     ]);
 })->name('tasks.show');
 
+
 Route::post('/tasks', function (Request $request) {
-  dd($request->all());
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+  ]);
+
+  $task = new Task;
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->save();
+  return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 
