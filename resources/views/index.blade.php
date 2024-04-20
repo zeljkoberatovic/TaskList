aeb22b571eb3486cd6e5aad199335c0f65393c6c
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-04-20 23:54:39 +0200

    Adding Pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,16 +3,21 @@
 @section('title', 'The list of tasks') 
 @section('content')
 
-    @if (count($tasks)) 
-        @forelse ($tasks as $task)
-            <div>
-                <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
-            </div>
-        @empty
+  @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id])}}">{{ $task->title }}</a>
+        </div>
+    @empty
             <div>There are not task!</div>
-        @endforelse
-    @endif 
+    @endforelse
+     
 
+    {{--Paginacija--}}
+    @if($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
 
 
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
     return view('index',[
-        'tasks' => Task::latest()->whereNotNull('completed')->get()
+        'tasks' => Task::latest()->paginate(10)
     ]);
 })->name('tasks.index');
 
